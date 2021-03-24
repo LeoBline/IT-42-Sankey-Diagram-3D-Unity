895752ffff0bae8c7ac21bba1596cd753dbceb03
zhuhy019 <491996029@qq.com>
2021-03-24 14:09:49 +0800

    111

---
+++ Assets/Scripts/Mouse.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Mouse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnMouseEnter()
+    {
+        string s = gameObject.name;
+        string[] sArray = s.Split('@');
+
+        transform.parent.parent.parent.Find("cameraCanvas").Find("Name").Find("NameText").GetComponent<Text>().text = sArray[0];
+        transform.parent.parent.parent.Find("cameraCanvas").Find("Information").Find("EnergyL").GetComponent<Text>().text = sArray[1];
+    }
+    private void OnMouseExit()
+    {
+        transform.parent.parent.parent.Find("cameraCanvas").Find("Name").Find("NameText").GetComponent<Text>().text = "";
+        transform.parent.parent.parent.Find("cameraCanvas").Find("Information").Find("EnergyL").GetComponent<Text>().text ="";
+
+    }
+}
